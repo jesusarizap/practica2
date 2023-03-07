0de230b854e9fccbabdde32c8bf3e06ffd1ef3fb
NicolasLeribleUMA <nicolas.lerible@uma.es>
2023-03-07 17:34:27 +0100

    + commented Nested tests structure

--- src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
+++ src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
@@ -4,4 +4,33 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DoublyLinkedListDequeTest {
+
+    /**
+     * Una lista vacia
+     *      no debe tener primer ni ultimo elemento (null)
+     *      debe tener longitud 0
+     *      cuando añade un elemento
+     *          al principio no tiene predecesor ni antecesor
+     *          al final no tiene predecesor ni antecesor
+     *      devuelve DoubleEndedQueueException si deleteFirt()
+     *      devuelve DoubleEndedQueueException si deleteLast()
+     *      devuelve DoubleEndedQueueException si first()
+     *      devuelve DoubleEndedQueueException si last()
+     *
+     * Una lista con al menos un elemento
+     *      debe tener primer y ultimo elemento
+     *      debe tener longitud correcta
+     *      añade un elemento corretamente
+     *          al principio
+     *          al final
+     *      borra elementos correctamente
+     *          al principio
+     *          al final
+     *      muestra correctamente
+     *          el primer elemento
+     *          el ultimo momento
+     *      puede recorrer del primer al ultimo elemento (¿en [size] pasos? como lo ve Jesus)
+     */
+
+
 }
