7ab6c80c085bcf8322912ab8a914f997597df2b1
NicolasLeribleUMA <nicolas.lerible@uma.es>
2023-03-07 12:25:36 +0100

    + implementado prepend()

--- src/main/java/org/mps/deque/DoublyLinkedListDeque.java
+++ src/main/java/org/mps/deque/DoublyLinkedListDeque.java
@@ -6,13 +6,21 @@ public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
     private DequeNode<T> last;
     private int size;
 
-    public DoublyLinkedListDeque() {
+    public DoublyLinkedListDeque(DequeNode<T> first, DequeNode<T> last) {
         // TODO
+        this.first = first;
+        this.last = last;
     }
 
     @Override
     public void prepend(T value) {
         // TODO
+        // Creo un nuevo nodo como primer elemento
+        DequeNode<T> newFirstNode = new DequeNode<>(value,null, this.first);
+        // Referencio al nuevo nodo como el anterior del actual primer nodo
+        this.first.setPrevious(newFirstNode);
+        // Sustituyo el rimer elemento
+        this.first = newFirstNode;
     }
 
     @Override
